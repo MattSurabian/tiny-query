e6c3add62b7d85dd5d542152e950d569b843c82a
Matt Surabian <matt@mattsurabian.com>
2014-03-11 19:19:50 -0400

    Initial commit

--- src/tiny-jquery.js
+++ src/tiny-jquery.js
@@ -1,2 +1,35 @@
+/**
+ * @param element
+ * @param context
+ */
+var $$ = function(element, context){
+    if(this instanceof $$){
+        var elements = null;
 
-// Your implementation here.
+        if(context){
+            elements = context.querySelectorAll(element);
+        }else{
+            elements = document.querySelectorAll(element);
+        }
+
+        this.length = elements.length;
+
+        for(var i =0; i< this.length; i++){
+            this[i] = elements[i];
+        }
+    }else{
+        return new $$(element, context);
+    }
+};
+
+$$.prototype = {
+    length: this.length,
+
+    forEach: function(fn){
+        for(var i=0; i<this.length; i++){
+            fn.call(this[i],this[i],i);
+        }
+    }
+};
+
+var tQuery = $$;
